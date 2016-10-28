85e8c1bd6f8f1f0129c2e6b5b9a4651721fdbffe
GRANT HOECHST <granthoechst@college.harvard.edu>
2016-10-27 23:33:17 -0400

    added the Unity project and builds

---
+++ Assets/Scripts/CombatMusicControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class CombatMusicControl : MonoBehaviour {
+
+	public AudioMixerSnapshot outOfCombat;
+	public AudioMixerSnapshot inCombat;
+	public AudioClip[] stings;
+	public AudioSource stingSource;
+	public float bpm = 120;
+
+	private float m_TransitionIn;
+	private float m_TransitionOut;
+	private float m_QuarterNote;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_QuarterNote = 60 / bpm;
+		m_TransitionIn = m_QuarterNote * 3;
+		m_TransitionOut = m_QuarterNote * 16;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Mystery"))
+		{
+			inCombat.TransitionTo (m_TransitionIn);
+		}
+		if (other.CompareTag ("Ping2"))
+		{
+			stingSource.clip = stings [0];
+			stingSource.Play ();
+			other.enabled = false;
+		}
+		if (other.CompareTag ("BGM1"))
+		{
+			stingSource.clip = stings [1];
+			stingSource.Play ();
+			other.enabled = false;
+		}
+		if (other.CompareTag ("Kapow"))
+		{
+			stingSource.clip = stings [2];
+			stingSource.Play ();
+			other.enabled = false;
+		}
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag ("Mystery"))
+		{
+			outOfCombat.TransitionTo (m_TransitionOut);
+		}
+
+	}
+}
---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// respond on collisions
+	void OnTriggerEnter(Collider mesh)
+	{
+		// only do stuff if hit by a projectile
+		if (mesh.gameObject.tag == "EnterMine") {
+			// call the NextLevel function in the game manager
+			Application.LoadLevel ("The Mine");
+		}
+	}
+}
